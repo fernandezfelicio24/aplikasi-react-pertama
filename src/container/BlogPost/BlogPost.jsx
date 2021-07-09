a20b2126580b6055f93b9e823666fd5eea50d5dc
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2021-07-09 14:52:30 +0700

    Interaction with back end, Calling API usign fetch and axios Part 2

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,13 +1,46 @@
 import React, {Component, Fragment} from 'react';
 import './BlogPost.css';
 import PostComp from '../../component/PostComp/PostComp';
+import axios from 'axios';
 class BlogPost extends Component {
+
+
+    state = {
+        post : []
+    }
+    componentDidMount(){
+
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        // .then(response => response.json())
+        // // .then(json => console.log(json))
+        // .then(json => {
+        //     this.setState({
+        //         post : json
+        //     })
+        // })
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((hasil) => {
+            // console.log(hasil);
+            // console.log(hasil.data);
+            this.setState({
+                post: hasil.data
+            })
+        })
+
+    }
+
         render(){
             return(
                 <Fragment>
                     <p className="section-title">Blog Post</p>
-                
-                    <PostComp title="title" desc="desc"/>
+                    {
+                        this.state.post.map(
+                            post => {
+                                
+                                return    <PostComp key={post.id} title={post.title} desc={post.body}/>
+                            })
+                    }
+            
                 </Fragment>
                 
             )
